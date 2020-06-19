c80c464602a85506b656ea52f8ae352b51d3e233
Satya S. Dev <satyasdev@2-9-SATYASYAHPUTRA.local>
2020-06-19 20:37:30 +0700

    Initial Commit - Create Project

---
+++ src/main/java/com/satyadara/mailsender/SpringBootMailApplication.java
@@ -0,0 +1,13 @@
+package com.satyadara.mailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMailApplication.class, args);
+    }
+
+}
